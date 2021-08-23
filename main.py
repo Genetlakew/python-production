b23b0a41ed8423ed49e6b9807279877a1e9e0f3d
Genetlakew <86495282+Genetlakew@users.noreply.github.com>
2021-08-23 21:25:05 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 # Write your code below this line 👇
+print('Hello world!')
+print('Hello World!\nHello world!\n')
+print('Hello' +' '+ 'Genet')
+print('Hello'+ ' '+'Genet' + ' ' + 'Lakew')
+
+print('Day 1 - Python print Function')
+print('The function is declared like this:')
+print("print('prin what to print')")
